c0858ed085ece813e2615b35bc50cc67ff8078ed
Topidesta a.k.a EmangDasar <desta.08b@gmail.com>
2020-02-13 14:22:43 +0700

    creat hooks for home komponen

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  API_URL,
+  API_KEY,
+  API_BASE_URL,
+  POSTER_SIZE,
+  BACKDROP_SIZE
+} from "../config";
+
+// Komponen
 import HeroImage from "./elements/HeroImage";
 import SearchBar from "./elements/SearchBar";
 import Grid from "./elements/Grid";
@@ -6,7 +15,13 @@ import MovieThumb from "./elements/MovieThumb";
 import LoadMoreBtn from "./elements/LoadMoreBtn";
 import Spinner from "./elements/Spinner";
 
+// custom hook
+import { useHomeFetch } from "./hooks/useHomeFetch";
+
 const Home = () => {
+  const [{ state, loading, error }, fetchMovies] = useHomeFetch();
+  console.log(state);
+
   return (
     <>
       <HeroImage />
