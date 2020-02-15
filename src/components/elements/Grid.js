76842e2f80796f2e5b653ac0e194fbfdcc79a6a5
Desta Muchamad <desta.muchamad@my.gavilan.edu>
2020-02-15 14:33:07 +0700

    grab all movie to grid

--- src/components/elements/Grid.js
+++ src/components/elements/Grid.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { StyledGrid, StyledGridContent } from "../styles/StyledGrid";
 
-const Grid = () => {
-  return <div>Grid</div>;
+const Grid = ({ Header, children }) => {
+  return (
+    <StyledGrid>
+      <h1>{Header}</h1>
+      <StyledGridContent>{children}</StyledGridContent>
+    </StyledGrid>
+  );
 };
 
 export default Grid;
